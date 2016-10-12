16b71146de557fbfa16561581b1526f3fad24cfb
universallp <universailp@web.de>
2016-10-12 19:54:16 +0200

    Removed unused dependencies

--- config.mk
+++ config.mk
@@ -9,8 +9,8 @@ X11LIB = /usr/X11R6/lib
 XINERAMALIBS = -L${X11LIB} -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
-INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags xft pango pangoxft libnotify`
-LIBS = -L/usr/lib -lc -lrt -L${X11LIB} -lX11 ${XINERAMALIBS} -lpthread `pkg-config --libs xft pango pangoxft libmpdclient libnotify`
+INCS = -I. -I/usr/include -I${X11INC} 
+LIBS = -L/usr/lib -lc -lrt -L${X11LIB} -lX11 ${XINERAMALIBS} `pkg-config --libs libmpdclient`
 
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} -D_FORTIFY_SOURCE=2 -DDEBUG
 CFLAGS = -std=gnu99 -Wall -march=native -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -O2 ${INCS} ${CPPFLAGS}
