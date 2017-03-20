becaabfcd77d988af196459cf1cf5d85da2041b5
universallp <universailp@web.de>
2017-03-20 15:35:52 +0100

    Removed mpd headers from repo and added them to make

--- config.mk
+++ config.mk
@@ -9,7 +9,7 @@ X11LIB = /usr/X11R6/lib
 XINERAMALIBS = -L${X11LIB} -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
-INCS = -I. -I/usr/include -I${X11INC} 
+INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/mpd/
 LIBS = -L/usr/lib -lc -lrt -L${X11LIB} -lX11 ${XINERAMALIBS} `pkg-config --libs libmpdclient`
 
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} -D_FORTIFY_SOURCE=2 -DDEBUG
